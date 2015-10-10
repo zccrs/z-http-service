66603c0ee3327d15987aa13565720e6d0f5f1a0d
zccrs <ccrr1314@live.com>
2015-10-10 17:46:13 +0800

    add httpinfo class

--- zhttpserver.cpp
+++ zhttpserver.cpp
@@ -4,15 +4,166 @@
 #include <QFileInfo>
 #include <QDir>
 #include <QStandardPaths>
+#include <QSettings>
 #include <QDebug>
 
 #include "zhttpserver.h"
 
 #define SERVERNAME "z-http"
+#define PORT 80
 
 const QString datapath = QDir::homePath().isEmpty() || QDir::homePath() == "/"
         ? "/root/."+QString(SERVERNAME)+"/data"
         : QDir::homePath() + "/."+QString(SERVERNAME)+"/data";
+
+HttpInfo::HttpInfo(const QByteArray &data, PackageType type):
+    m_httpVersion("HTTP/1.1"),
+    m_errorCode(NoError)
+{
+    m_url.setScheme("http");
+
+    QStringList list = QString::fromUtf8(data).split("\r\n");
+    if(!list.isEmpty()){
+        QByteArrayList tmp = list.first().toUtf8().split(' ');
+
+        if(tmp.count() == 3){
+            if(type == Request){
+                setMethod(tmp[0]);
+                QByteArray path = tmp[1].split('?').first();
+                m_url.setPath(path);
+                if(path.count() < tmp[1].count())
+                    m_url.setQuery(tmp[1].mid(path.count() + 1));
+                setHttpVersion(tmp[2]);
+            }else{
+                setHttpVersion(tmp[0]);
+                setError(ErrorCode(tmp[1].toInt()));
+                setErrorString(tmp[2]);
+            }
+        }else{
+            setError(BadRequest);
+            setErrorString(QString("%1 Bad Request").arg(BadRequest));
+            return;
+        }
+
+        int i = 1;
+
+        for(; i < tmp.count(); ++i){
+            tmp = list[i].toUtf8().split(':');
+            if(tmp.count() == 2){
+                setRawHeader(tmp.first(), tmp.last().trimmed());
+                if(tmp.first() == "Host")
+                    m_url.setHost(tmp.last().trimmed());
+            }else{
+                setError(BadRequest);
+                setErrorString(QString("%1 Bad Request").arg(BadRequest));
+                return;
+            }
+        }
+    }else{
+        setError(BadRequest);
+        setErrorString(QString("%1 Bad Request").arg(BadRequest));
+        return;
+    }
+}
+
+QByteArray HttpInfo::toRequestByteArray() const
+{
+    return method() + " " + m_url.path().toUtf8() + " " + httpVersion() + "\r\n" + toByteArray();
+}
+
+QByteArray HttpInfo::toReplyByteArray() const
+{
+    return httpVersion() + " " + QByteArray::number(error()) + " " + errorString().toUtf8() + "\r\n" + toByteArray();
+}
+
+QByteArray HttpInfo::rawHeader(const QByteArray &headerName) const
+{
+    return m_headers[headerName];
+}
+
+void HttpInfo::setRawHeader(const QByteArray &headerName, const QByteArray &headerValue)
+{
+    m_headers[headerName] = headerValue;
+}
+
+QByteArray HttpInfo::httpVersion() const
+{
+    return m_httpVersion;
+}
+
+void HttpInfo::setHttpVersion(const QByteArray &version)
+{
+    m_httpVersion = version;
+}
+
+QByteArray HttpInfo::method() const
+{
+    return m_method;
+}
+
+void HttpInfo::setMethod(const QByteArray &method)
+{
+    m_method = method;
+}
+
+QUrl &HttpInfo::url()
+{
+    return m_url;
+}
+
+HttpInfo::ErrorCode HttpInfo::error() const
+{
+    return m_errorCode;
+}
+
+void HttpInfo::setError(HttpInfo::ErrorCode error)
+{
+    m_errorCode = error;
+}
+
+QString HttpInfo::errorString() const
+{
+    return m_errorString;
+}
+
+void HttpInfo::setErrorString(const QString &str)
+{
+    m_errorString = str;
+}
+
+QByteArray HttpInfo::body() const
+{
+    return m_body;
+}
+
+void HttpInfo::setBody(const QByteArray &body)
+{
+    m_body = body;
+    setRawHeader("Content-Length", QByteArray::number(m_body.length()));
+}
+
+void HttpInfo::clear()
+{
+    m_headers.clear();
+    m_httpVersion.clear();
+    m_method.clear();
+    m_errorCode = NoError;
+    m_errorString.clear();
+    m_body.clear();
+}
+
+QByteArray HttpInfo::toByteArray() const
+{
+    QByteArray arr;
+    foreach (const QByteArray &key, m_headers.keys()) {
+        arr.append(key + ": " + m_headers.value(key) + "\r\n");
+    }
+
+    if(!m_body.isEmpty())
+        arr.append("\r\n" + m_body);
+
+    return arr;
+}
 
 ZHttpServer::ZHttpServer(QObject *parent) :
     QObject(parent),
@@ -27,7 +178,10 @@ ZHttpServer::~ZHttpServer()
 
 bool ZHttpServer::startServer()
 {
-    if(!m_tcpServer->listen(QHostAddress::Any, 80)){
+    if(m_tcpServer->isListening())
+        return true;
+
+    if(!m_tcpServer->listen(QHostAddress::Any, PORT)){
         qDebug() << "HttpServer: error: " << m_tcpServer->errorString();
         return false;
     }else{
@@ -38,39 +192,57 @@ bool ZHttpServer::startServer()
         qDebug() << "HttpServer: new connect:" << socket->peerAddress().toString() << socket->peerName() << socket->peerPort();
 
         connect(socket, &QTcpSocket::readyRead, [socket, this]{
-            QByteArray path_and_command = socket->readAll().split('\r').first().split(' ')[1];
-            QByteArray file_path = path_and_command.split('?').first();
-            QByteArray command = path_and_command == file_path ? "" : path_and_command.split('?').last();
-            QFileInfo fileInfo(datapath + file_path);
+            HttpInfo info(socket->readAll());
 
-            qDebug() << "Open file:" << fileInfo.absoluteFilePath();
+            qDebug() << info.url();
 
-            if(!fileInfo.absoluteFilePath().contains(datapath)){
-                socket->write(messagePackage("", "text/html",  UnauthorizedAccessError, "Unauthorized Access"));
-                return;
-            }
-
-            QFile file;
+            QByteArray command = info.url().query().toUtf8();
+            QFileInfo fileInfo(datapath + info.url().path());
 
             qDebug() << "HttpServer: command:" << command.split('&');
 
-            if(fileInfo.isFile()){
-                file.setFileName(fileInfo.absoluteFilePath());
-            }else if(fileInfo.isDir()){
-                file.setFileName(fileInfo.filePath().append("default.html"));
-            }
+            do{
+                if(!fileInfo.absoluteFilePath().contains(datapath)){
+                    socket->write(messagePackage("", "text/html",  HttpInfo::UnauthorizedAccessError, "Unauthorized Access"));
+                    break;
+                }
 
-            if(!file.exists()){
-                socket->write(messagePackage("", "text/html",  FileNotFoundError, "File Not Found"));
-                return;
-            }
+                QFile file;
 
-            if(file.open(QIODevice::ReadOnly)){
-                socket->write(messagePackage(file.readAll()));
-            }else{
-                qDebug() << "Open file failed:" << file.fileName() << "error:" << file.errorString();
-                socket->write(messagePackage("", "text/html", OtherError, file.errorString()));
-            }
+                if(fileInfo.isFile()){
+                    file.setFileName(fileInfo.absoluteFilePath());
+                }else if(fileInfo.isDir()){
+                    QSettings setting(fileInfo.absolutePath().append("/.config"));
+                    QString jump = setting.value("jump").toString();
+                    if(jump.isEmpty()){
+                        file.setFileName(fileInfo.absolutePath().append(setting.value("default", "default.html").toString()));
+                    }else{
+                        QDir dir = fileInfo.dir();
+                        if(dir.cd(jump)){
+                            info.url().setPath(dir.absolutePath().replace(datapath, ""));
+                            socket->write(getJumpPackage(info.url().toString().toUtf8()));
+                            break;
+                        }else{
+                            socket->write(messagePackage("", "text/Html", HttpInfo::UnknowError, QString("Jump to %1 failed").arg(jump)));
+                            break;
+                        }
+                    }
+                }
+
+                qDebug() << "Open file:" << fileInfo.absoluteFilePath();
+
+                if(!file.exists()){
+                    socket->write(messagePackage("", "text/html",  HttpInfo::FileNotFoundError, "File Not Found"));
+                    break;
+                }
+
+                if(file.open(QIODevice::ReadOnly)){
+                    socket->write(messagePackage(file.readAll()));
+                }else{
+                    qDebug() << "Open file failed:" << file.fileName() << "error:" << file.errorString();
+                    socket->write(messagePackage("", "text/html", HttpInfo::OtherError, file.errorString()));
+                }
+            }while(false);
 
             socket->close();
         });
@@ -88,26 +260,37 @@ void ZHttpServer::stopServer()
     m_tcpServer->close();
 }
 
-QByteArray ZHttpServer::messagePackage(QString content, const QString &content_type,
-                                       ErrorCode error_code, const QString &error_message) const
+QByteArray ZHttpServer::messagePackage(QByteArray content, const QByteArray &content_type,
+                                       HttpInfo::ErrorCode error_code, const QString &error_message) const
 {
-    QString message;
-
-    if(error_code != NoError)
+    if(error_code != HttpInfo::NoError)
         content = getErrorHtml(error_code, error_message);
 
-    message = message.append("HTTP/1.1 %1 %2\r\n").arg(error_code).arg(error_message);
-    message = message.append("Content-Type: %1\r\n").arg(content_type);
-    message = message.append("Content-Length: %1\r\n").arg(content.length());
-    message = message.append("Connection: Close\r\n");
+    HttpInfo re;
 
-    if(!content.isEmpty())
-        message = message.append("\r\n%1").arg(content);
+    re.setError(error_code);
+    re.setErrorString(error_message);
+    re.setRawHeader("Content-Type", content_type);
+    re.setRawHeader("Connection", "Close");
+    re.setBody(content);
 
-    return message.toUtf8();
+    return re.toReplyByteArray();
 }
 
-QString ZHttpServer::getErrorHtml(ErrorCode error_code, const QString &error_message) const
+QByteArray ZHttpServer::getErrorHtml(HttpInfo::ErrorCode error_code, const QString &error_message) const
 {
-    return QString("<html><head><title>%1 %2</title></head><body bgcolor=\"white\"><center><h1>%1 %2</h1></center><hr></body></html>").arg(error_code).arg(error_message);
+    return QString("<html><head><title>%1 %2</title></head><body bgcolor=\"white\"><center><h1>%1 %2</h1></center><hr></body></html>").arg(error_code).arg(error_message).toUtf8();
 }
+
+QByteArray ZHttpServer::getJumpPackage(const QByteArray &target_path) const
+{
+    HttpInfo re;
+
+    re.setError(HttpInfo::TemporarilyMoved);
+    re.setErrorString("Found");
+    re.setRawHeader("Connection", "Close");
+    re.setRawHeader("Location", target_path);
+
+    return re.toReplyByteArray();
+}
+
--- zhttpserver.h
+++ zhttpserver.h
@@ -2,6 +2,65 @@
 #define ZHTTPSERVER_H
 
 #include <QObject>
+#include <QMap>
+#include <QUrl>
+
+class HttpInfo
+{
+public:
+    enum PackageType{
+        Request,
+        Reply
+    };
+
+    enum ErrorCode{
+        NoError = 200,
+        TemporarilyMoved = 302,
+        BadRequest = 400,
+        UnauthorizedAccessError = 403,
+        FileNotFoundError = 404,
+        UnknowError = 500,
+        OtherError = 503
+    };
+
+    HttpInfo(const QByteArray & data = "", PackageType type = Request);
+
+    QByteArray toRequestByteArray() const;
+    QByteArray toReplyByteArray() const;
+
+    QByteArray rawHeader(const QByteArray &headerName) const;
+    void setRawHeader(const QByteArray &headerName, const QByteArray &headerValue);
+
+    QByteArray httpVersion() const;
+    void setHttpVersion(const QByteArray &version);
+
+    QByteArray method() const;
+    void setMethod(const QByteArray &method);
+
+    QUrl &url();
+
+    ErrorCode error() const;
+    void setError(ErrorCode error);
+
+    QString errorString() const;
+    void setErrorString(const QString &str);
+
+    QByteArray body() const;
+    void setBody(const QByteArray &body);
+
+    void clear();
+
+private:
+    QMap<QByteArray, QByteArray> m_headers;
+    QByteArray m_httpVersion;
+    QByteArray m_method;
+    QUrl m_url;
+    ErrorCode m_errorCode;
+    QString m_errorString;
+    QByteArray m_body;
+
+    QByteArray toByteArray() const;
+};
 
 class QTcpServer;
 class ZHttpServer : public QObject
@@ -11,23 +70,16 @@ public:
     explicit ZHttpServer(QObject *parent = 0);
     ~ZHttpServer();
 
-    enum ErrorCode{
-        NoError = 200,
-        UnauthorizedAccessError = 403,
-        FileNotFoundError = 404,
-        UnknowError = 500,
-        OtherError = 503
-    };
-
     bool startServer();
     void stopServer();
 
 private:
     QTcpServer *m_tcpServer;
 
-    QByteArray messagePackage(QString content, const QString &content_type = "text/html",
-                           ErrorCode error_code = NoError, const QString &error_message = "") const;
-    QString getErrorHtml(ErrorCode error_code, const QString &error_message) const;
+    QByteArray messagePackage(QByteArray content, const QByteArray &content_type = "text/html",
+                           HttpInfo::ErrorCode error_code = HttpInfo::NoError, const QString &error_message = "") const;
+    QByteArray getErrorHtml(HttpInfo::ErrorCode error_code, const QString &error_message) const;
+    QByteArray getJumpPackage(const QByteArray &target_path) const;
 };
 
 #endif // ZHTTPSERVER_H
